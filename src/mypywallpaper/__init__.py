01b513022529c935897bcc5410d911010e51652b
tct123 <42028373+tct123@users.noreply.github.com>
2025-08-25 06:06:31 +0200

    Update __init__.py

--- src/mypywallpaper/__init__.py
+++ src/mypywallpaper/__init__.py
@@ -6,6 +6,22 @@ def get_de():
     desktop_env = os.environ.get("XDG_CURRENT_DESKTOP").lower()
     if not desktop_env:
         desktop_env = os.environ.get("DESKTOP_SESSION").lower()
+    if "kde" in desktop_env or "plasma" in desktop_env:
+        return "kde"
+    elif "gnome" in desktop_env:
+        return "gnome"
+    elif "xfce" in desktop_env:
+        return "xfce"
+    elif "lxde" in desktop_env:
+        return "lxde"
+    elif "cinnamon" in desktop_env:
+        return "cinnamon"
+    elif "mate" in desktop_env:
+        return "mate"
+    elif "unity" in desktop_env:
+        return "unity"
+    else:
+        return "unknown"
 
 
 if system() == "Windows":
@@ -35,24 +51,38 @@ elif system() == "Linux":
         return out
 
     try:
-        v = int(get_output("gnome-session --version").split()[-1][0])
-        if v == 2:
+        de = get_de()
+        if de == "gnome":
+            v = int(get_output("gnome-session --version").split()[-1][0])
+            if v == 2:
+
+                def change_wallpaper(uri):
+                    subprocess.run(
+                        [
+                            "gconftool-2",
+                            "--type=string",
+                            -"-set",
+                            "/desktop/gnome/background/picture_filename",
+                            uri,
+                        ]
+                    )
+
+            elif v == 3:
+
+                def change_wallpaper(uri):
+                    subprocess.run(
+                        [
+                            "gsettings",
+                            "set",
+                            "org.gnome.desktop.background",
+                            "picture-uri" f'"file://{uri}"',
+                        ]
+                    )
+
+        if de == "kde":
 
             def change_wallpaper(uri):
-                s(
-                    "gconftool-2 --type=string --set /desktop/gnome/background/picture_filename {0}".format(
-                        uri
-                    )
-                )
-
-        elif v == 3:
-
-            def change_wallpaper(uri):
-                s(
-                    'gsettings set org.gnome.desktop.background picture-uri "file://{0}"'.format(
-                        uri
-                    )
-                )
+                subprocess.run(["plasma-apply-wallpaperimage", uri], check=True)
 
     except:
         raise OSError("Wallaper Change is supported in GNOME 2/3 and Unity only")
